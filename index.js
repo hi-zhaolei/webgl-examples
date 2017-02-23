3045366e06eed22232c99b0241ece2945d9afb4e
Lei Zhao <leozhao.go@gmail.com>
2017-02-23 18:40:51 +0800

    lesson 2

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ var bodyParser = require('body-parser')
 var app = express();
 var path = require('path')
 
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('static'))
-app.get('/', function(req, res){
-	res.sendFile('FIXME',function(){
-	})
-});
-app.get('/lesson/:lid', function(req, res){
-	var lid = req.params.lid;
-	var filepath = path.join(__dirname,`examples/lesson${lid}/index.html`);
+
+app.get('*', function(req, res){
+	var filepath = path.join(__dirname, './', req.path)	
+	if( req.path.indexOf('.') === -1 ) {
+		filepath = path.join( filepath, 'index.html' )
+	} 
 	res.sendFile(filepath, function (err) {
 		if(err){
 			console.log(err)
 			res.status(err.status).end()
 		} else {
-			console.log(`send file lesson${lid}`)
+			console.log(`send file ${filepath}`)
 		}
 	})
 })
