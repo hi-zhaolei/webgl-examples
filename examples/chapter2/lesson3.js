7b139f3138f8cc8a7bcabc7460ee8b0518696fc1
Lei Zhao <leozhao.go@gmail.com>
2017-02-24 14:54:24 +0800

    add lesson 3 & 4

---
+++ examples/chapter2/lesson3.js
@@ -0,0 +1,66 @@
+function main () {
+
+	// 获取cavas元素
+	var canvas = document.getElementById('myCanvas')
+	if(!canvas){
+		console.log('Failed to retrieve the <canvas> element')
+		return;
+	}
+	
+	// 获取绘制二维图形绘图上下文
+	//var ctx = canvas.getContext('2d')
+	// 获取WebGL绘图上下文
+	var gl = getWebGLContext(canvas)
+
+	if(!gl){
+		console.log('Failed to get the rendering context for WebGL')
+		return;
+	}
+
+  if( !initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE ) ){
+    console.log('Failed to initialize shaders')
+    return;
+  }
+
+	// 获取attribute变量地址
+	var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+	var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+	// 获取uniform变量地址
+	var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+
+	// 数据传递
+	gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0)
+	gl.vertexAttrib1f(a_PointSize, 10.0)
+	gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0)
+
+	// 指定清空canvas颜色
+	gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+	// 清空canvas
+	gl.clear(gl.COLOR_BUFFER_BIT)
+
+  // 绘制
+  gl.drawArrays(gl.POINTS, 0, 1)
+
+}
+
+const VSHADER_SOURCE =
+	'precision mediump float;\n' +
+	'attribute vec4 a_Position;\n' +
+	'attribute float a_PointSize;\n' +
+  'void main() {\n' +
+  ' gl_Position = a_Position;\n' +
+  ' gl_PointSize = a_PointSize;\n' +
+  '}\n';
+
+const FSHADER_SOURCE =
+	'precision mediump float;\n' +
+	'uniform vec4 u_FragColor;\n' +
+  'void main() {\n' +
+  ' gl_FragColor = u_FragColor;\n' +
+  '}\n';
+
+
+
+
+main()
